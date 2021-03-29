7493d09cb57176c7f21bc5e5a96be0203cd66696
Nick Hennessy <njhnny@gmail.com>
2021-03-28 17:08:44 -0700

    end of day commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-// import Sound from './assets/audio/restaurant.mp3';
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+//import testMp3 from "assets/audio/restaurant.mp3";
+let song = new Audio("assets/audio/restaurant.mp3");
+//let filename = "restaurant";
+
+$("#submit").click(function(event) {
+  event.preventDefault();
+  
+  song.volume = 0.5;
+  //$('audio#audio1')[0].play();
+  //restaurant.loop = true;
+  //restaurant.currentTime = 0;
+  song.play();
+//   restaurant.pause();
+});
