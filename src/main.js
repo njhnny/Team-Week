7b9162eda569dfce51590f359256368914f0f8eb
Nick Hennessy <njhnny@gmail.com>
2021-03-29 18:51:08 -0700

    create pause button
    Co-authored-by: sinkstyt <sinkstyt@users.noreply.github.com>
    Co-authored-by: HR-Williams <HR-Williams@users.noreply.github.com>
    Co-authored-by: GARRETT PAUL BROWN <GBProductions@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import './assets/audio/restaurant.mp3';
 //import testMp3 from "assets/audio/restaurant.mp3";
 let song = new Audio("assets/audio/restaurant.mp3");
 //let filename = "restaurant";
@@ -16,3 +17,20 @@ $("#submit").click(function(event) {
   song.play();
 //   restaurant.pause();
 });
+$("#pause").click(function(event) {
+  event.preventDefault();
+  song.pause();
+});
+
+// $('#audioSlider').slider({
+//     orientation: "vertical",
+//     value: audio1.volume,
+//     min: 0,
+//     max: 1,
+//     range: 'min',
+//     animate: true,
+//     step: .1,
+//     slide: function(e, ui) {
+//     audio1.volume = ui.value;
+//     }
+// });
