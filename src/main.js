379dbc8b60befda362347714a92e51d3c499ff41
Nick Hennessy <njhnny@gmail.com>
2021-03-30 20:48:56 -0700

    connect to web audio api
    Co-authored-by: HR-Williams <HR-Williams@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import './assets/audio/bell.mp3';
 let song = new Audio("assets/audio/restaurant.mp3");
 let bell = new Audio("assets/audio/bell.mp3");
 
-const audioContext = new AudioContext();
 
 $("#play").click(function(event) {
   event.preventDefault();
@@ -31,3 +30,44 @@ $("#pausebell").click(function(event) {
   event.preventDefault();
   bell.pause();
 });
+
+
+// Below here, experimenting with Web-Audio-API
+
+// for legacy browsers
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const audioContext = new AudioContext();
+
+// get the audio element
+const audioElement = document.querySelector('audio');
+
+// pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+// select our play button
+const playButton = document.querySelector('button');
+
+playButton.addEventListener('click', function() {
+
+  // check if context is in suspended state (autoplay policy)
+  if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
+
+  // play or pause track depending on state
+  if (this.dataset.playing === 'false') {
+    audioElement.play();
+    this.dataset.playing = 'true';
+  } else if (this.dataset.playing === 'true') {
+    audioElement.pause();
+    this.dataset.playing = 'false';
+  }
+
+}, false);
+
+audioElement.addEventListener('ended', () => {
+  playButton.dataset.playing = 'false';
+}, false);
