f7d76106ce82e0c1fc00c18bb66720bde90d53b5
Nick Hennessy <njhnny@gmail.com>
2021-04-01 00:16:43 -0700

    instantiate vr view after the page loads

--- src/main.js
+++ src/main.js
@@ -29,3 +29,13 @@ $("#pausebell").click(function(event) {
   event.preventDefault();
   bell.pause();
 });
+
+window.addEventListener('load', onVrViewLoad);
+
+function onVrViewLoad() {
+  // Selector '#vrview' finds element with id 'vrview'.
+  var vrView = new VRView.Player('#vrview', {
+    video: '/url/to/video.mp4',
+    is_stereo: true
+  });
+}
