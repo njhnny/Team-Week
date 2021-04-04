fcc11b6fa54a4bfd4a61d62546646cc28732649e
Nick Hennessy <njhnny@gmail.com>
2021-04-04 10:44:46 -0700

    add audio files
    Co-authored-by: sinkstyt <sinkstyt@users.noreply.github.com>
    Co-authored-by: HR-Williams <HR-Williams@users.noreply.github.com>
    Co-authored-by: GARRETT PAUL BROWN <GBProductions@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import './assets/audio/restaurant.mp3';
 import './assets/audio/bell.mp3';
+import './assets/audio/forest1.mp3';
+import './assets/audio/forest1.ogg';
 import 'vrview.js';
 let song = new Audio("assets/audio/restaurant.mp3");
 let bell = new Audio("assets/audio/bell.mp3");
+let forestOgg = new Audio('assets/audio/forest1.ogg');
+let forestMp3 = new Audio('assets/audio/forest1.mp3');
 
 $("#play").click(function(event) {
   event.preventDefault();
@@ -31,15 +35,20 @@ $("#pausebell").click(function(event) {
   bell.pause();
 });
 
-window.addEventListener('load', onVrViewLoad);
+$('#play-forest-ogg').click(function(event) {
+  event.preventDefault();
+  forestOgg
+});
 
-function onVrViewLoad() {
-  // Selector '#vrview' finds element with id 'vrview'.
-  var vrView = new VRView.Player('#vrview', {
-    image: 'assets/images/bluffs360.jpg',
-    is_stereo: false,
-    width: "100%",
-    height: "100%"
-  });
-}
+// window.addEventListener('load', onVrViewLoad);
+
+// function onVrViewLoad() {
+//   // Selector '#vrview' finds element with id 'vrview'.
+//   var vrView = new VRView.Player('#vrview', {
+//     image: 'assets/images/bluffs360.jpg',
+//     is_stereo: false,
+//     width: "100%",
+//     height: "100%"
+//   });
+// }
 
